70fad5dc4077b53af8a66039b4445119407b4cbd
Denny Tsai <happydenn@happydenn.net>
2017-05-17 22:33:52 +0800

    add mp3 download button using service from youtubeinmp3.com

--- mixerbox-plus.user.js
+++ mixerbox-plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MixerBox+
 // @namespace    https://github.com/happydenn/mixerbox-plus
-// @version      0.1
+// @version      0.2.0
 // @description  Custom controls for MixerBox Web.
 // @author       happydenn
 // @grant        none
@@ -18,7 +18,7 @@ function initScript() {
 }
 
 function onMixerBoxLoaded() {
-  console.log('MixerBox+ by happydenn. Version 0.1 (20170517)');
+  console.log('MixerBox+ by happydenn. Version 0.2.0 (20170517)');
   console.log('MixerBox loaded.');
 
   var iframe = document.getElementById('MB-player-iframe');
@@ -28,6 +28,49 @@ function onMixerBoxLoaded() {
   }
 
   iframe.setAttribute('allowfullscreen', '');
+  setInterval(detectPlaylistItems, 350);
+}
+
+function detectPlaylistItems() {
+  var result = document.querySelectorAll('.playlist-item-container:not(.mbplus-annotated)');
+
+  if (result.length === 0) {
+    return;
+  }
+
+  result.forEach(function(playlist) {
+    var songRows = playlist.querySelectorAll('.song-row');
+
+    songRows.forEach(function(row) {
+      var imgEl = row.querySelector('.thumbnailContainer > img');
+      var videoId = imgEl.src.match(/.+:\/\/i\.ytimg\.com\/vi\/(.+)\/hqdefault\.jpg/i)[1];
+
+      var titleEl = row.querySelector('.title');
+      var mp3Button = createDownloadMP3Button(videoId);
+      titleEl.appendChild(mp3Button);
+    });
+
+    playlist.classList.add('mbplus-annotated');
+  });
+}
+
+function createDownloadMP3Button(videoId) {
+  var mp3Button = document.createElement('a');
+  mp3Button.setAttribute('href', '#');
+  mp3Button.textContent = 'Download MP3';
+
+  mp3Button.style.marginLeft = '10px';
+  mp3Button.style.display = 'inline-block';
+  mp3Button.style.borderBottom = '1px dotted #37b9eb';
+  mp3Button.style.color = '#37b9eb';
+
+  mp3Button.addEventListener('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    window.open('https://www.youtubeinmp3.com/fetch/?video=' + encodeURIComponent('https://www.youtube.com/watch?v=' + videoId));
+  });
+
+  return mp3Button;
 }
 
 function onPlayerFrameLoaded() {
