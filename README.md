2bcc10f1b9ba57688e83043edbf5465ba783f833
Denny Tsai <happydenn@happydenn.net>
2017-05-17 11:10:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# MixerBox+
+
+讓在瀏覽器裡面的 MixerBox 更好用！
+
+## 功能
+
+* 重新啟用播放器的播放控制列
+* 啟用播放器的全螢幕功能
+* 自動切換影片畫質為最高
+
+## 安裝
+
+本 userscript 相容於各大瀏覽器以及各 script manager。請先在你的瀏覽器安裝 script manager 之後再將本 script 加入就可以啦！如果是使用 Chrome 的人推薦使用 [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=zh-TW)
+
+## 關於
+
+本 script 目前是由小弟某天晚上想把 MixerBox 當成點唱機使用但是發現竟然沒辦法全螢幕播放因此產生的 tweak。
+
+## Contributing
+
+如果有發現任何問題歡迎發 issue 或者 pull request 給我！
+
+## 特別感謝
+
+* [MixerBox](http://www.mixerbox.com/) - 提供超棒的音樂影片播放清單和服務，他們的 App 也很棒喔！
---
+++ mixerbox-plus.user.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name         MixerBox+
+// @namespace    https://mb-plus.hpd.io
+// @version      0.1
+// @description  Custom controls for MixerBox Web.
+// @author       happydenn
+// @grant        none
+// @match        *://www.mixerbox.com/*
+// @match        *://spider1.cdn-network1.com/*
+// ==/UserScript==
+
+function initScript() {
+  if (window.location.host.includes('cdn-network1.com')) {
+    onPlayerFrameLoaded();
+  } else if (window.location.host.includes('mixerbox.com')) {
+    onMixerBoxLoaded();
+  }
+}
+
+function onMixerBoxLoaded() {
+  console.log('MixerBox+ by happydenn. Version 0.1 (20170517)');
+  console.log('MixerBox loaded.');
+
+  var iframe = document.getElementById('MB-player-iframe');
+
+  if (!iframe) {
+    return;
+  }
+
+  iframe.setAttribute('allowfullscreen', '');
+}
+
+function onPlayerFrameLoaded() {
+  console.log('Player frame loaded.');
+
+  window.onYouTubeIframeAPIReady = function() {
+    window.MBYTPlayer = new YT.Player('yt-player-div', {
+      height: '100%',
+      width: '100%',
+      playerVars: {
+        'playsinline': 1,
+        'controls': 1,
+        'showinfo': 0,
+        'rel': 0,
+        'start': 0,
+        'enablejsapi': 1,
+        'iv_load_policy': 3,
+        'origin': window.MB_DOMAIN_NAME
+      },
+      events: {
+        'onReady': window.onPlayerReady,
+        'onStateChange': window.onPlayerStateChange,
+        'onError': window.onPlayerError
+      }
+    });
+
+    var player = window.MBYTPlayer;
+
+    player.addEventListener('onStateChange', function(event) {
+      if (event.data === YT.PlayerState.PLAYING) {
+        var levels = player.getAvailableQualityLevels();
+        var currentLevel = player.getPlaybackQuality();
+
+        if (levels[0] !== currentLevel) {
+          console.log('Automatically changing quality level to ' + levels[0]);
+          player.setPlaybackQuality(levels[0]);
+        }
+      }
+    });
+  };
+}
+
+initScript();
